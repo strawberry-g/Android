dff17207a25504ba32bd60f0c41934877e9457da
strawberry <1535949731@qq.com>
2019-09-06 15:10:26 +0800

    HelloWorld Android

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.helloworld;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.example.helloworld.listview.ListViewActivity;
 
 public class MainActivity extends AppCompatActivity {
     //声明空间
+    private Button myBd;
     private Button myButtonTextView;
     private Button myButton;
     private Button myEditText;
     private Button myRadioButton;
     private Button myCheckBox;
+    private Button myImgView;
+    private Button myListView;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -26,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         myEditText = findViewById(R.id.btn_edittext);
         myRadioButton = findViewById(R.id.btn_radio);
         myCheckBox = findViewById(R.id.btn_checkBox);
+        myBd = findViewById(R.id.btn_bdyx);
+        myImgView = findViewById(R.id.btn_img_view);
+        myListView = findViewById(R.id.btn_list_view);
         setOnClickListeners();
     }
 
@@ -36,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
         myEditText.setOnClickListener(onClick);
         myRadioButton.setOnClickListener(onClick);
         myCheckBox.setOnClickListener(onClick);
+        myImgView.setOnClickListener(onClick);
+        myBd.setOnClickListener(onClick);
+        myListView.setOnClickListener(onClick);
     }
 
     private class OnClick implements View.OnClickListener{
@@ -62,6 +73,18 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.btn_checkBox:
                     //跳转到CheckBox演示页面
                     intent = new Intent(MainActivity.this,CheckBoxActivity.class);
+                    break;
+                case R.id.btn_bdyx:
+                    //跳转到百度首页
+                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.baidu.com"));
+                    break;
+                case R.id.btn_img_view:
+                    //跳转到ImageView演示页面
+                    intent = new Intent(MainActivity.this,ImageViewActivity.class);
+                    break;
+                case R.id.btn_list_view:
+                    //跳转到ListView演示页面
+                    intent = new Intent(MainActivity.this, ListViewActivity.class);
                     break;
             }
             startActivity(intent);
