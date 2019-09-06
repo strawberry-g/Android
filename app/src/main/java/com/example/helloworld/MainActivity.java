e9fec4be6e4be4222f19a1c36a816c250d9b0cdd
strawberry <1535949731@qq.com>
2019-09-06 16:04:32 +0800

    HelloWorld Android

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import com.example.helloworld.listview.ListViewActivity;
 
+//应用程序文件，将被转化为Dalvik可执行文件并运行
 public class MainActivity extends AppCompatActivity {
     //声明空间
     private Button myBd;
@@ -19,10 +20,14 @@ public class MainActivity extends AppCompatActivity {
     private Button myImgView;
     private Button myListView;
 
+    //onCreate方法activity创建时调用，是一个activity声明周期的开始
+    //Bundle类型的数据与Map类型的数据相似，都是以key-value的形式存储数据的。
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //指定当前Activity的布局文件为res/layout/main.xml
+        //指定当前Activity的布局文件为res/layout/activity_main.xml
+        //gen/com.example.helloworld/R.java文件是活动的Java文件（如MainActivity.java）和资源（如strings.xml）之间的胶水。
+        //这是一个自动生成的文件，不要修改R.java文件的内容
         setContentView(R.layout.activity_main);
 
         //找到空间
