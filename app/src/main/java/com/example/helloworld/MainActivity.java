528cdf2f0ef1589d2c77ba6c3fcee2e2ce8c3021
strawberry <1535949731@qq.com>
2019-09-08 15:20:47 +0800

    HelloWorld Android

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,23 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import com.example.helloworld.gridview.GridViewActivity;
 import com.example.helloworld.listview.ListViewActivity;
+import com.example.helloworld.recyclerview.RecyclerViewActivity;
 
 //应用程序文件，将被转化为Dalvik可执行文件并运行
 public class MainActivity extends AppCompatActivity {
-    //声明空间
-    private Button myBd;
-    private Button myButtonTextView;
-    private Button myButton;
-    private Button myEditText;
-    private Button myRadioButton;
-    private Button myCheckBox;
-    private Button myImgView;
-    private Button myListView;
-    private Button myGridV;
-    private Button myTest;
+    //每个控件声明空间
+    private Button myBd;//跳转到指定网页
+    private Button myButtonTextView;//文字页面
+    private Button myButton;//按钮
+    private Button myEditText;//输入框、文本框
+    private Button myRadioButton;//单选框
+    private Button myCheckBox;//复选框
+    private Button myImgView;//图片
+    private Button myListView;//列表视图
+    private Button myGridV;//网格视图
+    private Button myTest;//传递参数
+    private Button myRv;//通用视图
 
     //onCreate方法activity创建时调用，是一个activity声明周期的开始
     //Bundle类型的数据与Map类型的数据相似，都是以key-value的形式存储数据的。
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         //活动类从项目的res/layout中的XML文件加载所有的UI组件
         setContentView(R.layout.activity_main);
 
-        //找到空间
+        //findViewById根据控件自定义的id找到空间
         myButtonTextView = findViewById(R.id.btn_textview);
         myButton = findViewById(R.id.btn_button);
         myEditText = findViewById(R.id.btn_edittext);
@@ -46,11 +47,15 @@ public class MainActivity extends AppCompatActivity {
         myListView = findViewById(R.id.btn_list_view);
         myGridV = findViewById(R.id.btn_grid_view);
         myTest = findViewById(R.id.btn_test);
-        setOnClickListeners();
+        myRv = findViewById(R.id.btn_rv);
+
+        setOnClickListeners();//调用下面的点击事件方法
     }
 
+    //点击事件
     private void setOnClickListeners(){
         OnClick onClick = new OnClick();
+        //每个控件设置监听点击事件
         myButtonTextView.setOnClickListener(onClick);
         myButton.setOnClickListener(onClick);
         myEditText.setOnClickListener(onClick);
@@ -61,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
         myListView.setOnClickListener(onClick);
         myGridV.setOnClickListener(onClick);
         myTest.setOnClickListener(onClick);
+        myRv.setOnClickListener(onClick);
     }
 
     private class OnClick implements View.OnClickListener{
@@ -112,9 +118,13 @@ public class MainActivity extends AppCompatActivity {
                     b.putString("name","Android");
                     b.putInt("age",18);
                     intent.putExtras(b);*/
-                    //第二种intent.putExtra("data",b)
+                    //第二种intent.putExtras("data",b)
 
                     intent.putExtra("user",new User("Android",20));
+                    break;
+                case R.id.btn_rv:
+                    //跳转到RecyclerView页面
+                    intent = new Intent(MainActivity.this, RecyclerViewActivity.class);
                     break;
             }
             startActivity(intent);
--- app/src/main/java/com/example/helloworld/TextViewActivity.java
+++ app/src/main/java/com/example/helloworld/TextViewActivity.java
@@ -7,8 +7,8 @@ import android.text.Html;
 import android.widget.TextView;
 
 public class TextViewActivity extends AppCompatActivity {
-    //声明空间
-    //private TextView myTextView4,myTextView5;
+    //声明空间 为4、5、6的文字设置效果
+    //可以一起定义private TextView myTextView4,myTextView5;
     private TextView myTextView4;
     private TextView myTextView5;
     private TextView myTextView6;
@@ -17,18 +17,17 @@ public class TextViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_view);
+
         //找到空间
         myTextView4 = findViewById(R.id.tv_4);
+        myTextView5 = findViewById(R.id.tv_5);
+        myTextView6 = findViewById(R.id.tv_6);
         //中划线
         myTextView4.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
         //去除锯齿
         myTextView4.getPaint().setAntiAlias(true);
-
-        myTextView5 = findViewById(R.id.tv_5);
         //下划线
         myTextView5.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
-
-        myTextView6 = findViewById(R.id.tv_6);
         //下划线
         myTextView6.setText(Html.fromHtml("<u>潇潇暮雨子规啼</u>"));
     }
